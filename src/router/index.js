e0be2cbf7e4a5f4c33baeb0982bdd726283f3a1d
gaetan16 <gaetan@stock-pro.fr>
2022-12-19 14:34:51 +0100

    Remove AboutView.vue && create/add to navigation LoginView/RegisterView

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RegisterView from "@/views/authentication/RegisterView.vue";
+import LoginView from "@/views/authentication/LoginView.vue";
 
 const routes = [
   {
@@ -8,13 +10,23 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/signup',
+    name: 'signup',
+    component: RegisterView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  //{
+    //path: '/about',
+    //name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    //component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  //}
 ]
 
 const router = createRouter({
