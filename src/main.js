8c7d2840531e0da2d473db0bc62efb2dcde8e8d5
Drexal72 <alexandre.ortuno@tutanota.com>
2023-01-02 17:38:43 +0100

    install axentix by npm install && starting to refactor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import VueAxentix from "@axentix/vue";
+import '@axentix/vue/dist/vue3/vue-axentix.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App).use(VueAxentix).use(store).use(router).mount('#app')
--- src/views/authentication/LoginView.vue
+++ src/views/authentication/LoginView.vue
@@ -1,58 +1,43 @@
 <template>
   <div class="login-main-container">
-    <form class="form-material">
-      <div class="form-field">
-        <label for="name2">Name</label>
-        <input type="text" id="name2" class="form-control rounded-1" placeholder="Axentix" />
-        <span class="form-helper">This is a helper text.</span>
-      </div>
-    </form>
-    <form class="form-material form-material-bordered">
+    <div class="form">
       <div class="login-header">
         <img style="float: left" :src="require('@/assets/arrow_back.png')" width="24"  alt=""/>
         <span class="font-s6">Connexion</span>
         <span></span>
       </div>
 
-      <div class="form-field">
-        <label for="email">Email</label>
-        <input
-            type="email"
-            id="email"
-            class="form-control rounded-1"
-            :value="email"
-            @input="e => email = e.target.value"/>
-        <span class="form-helper">This is a helper text.</span>
-      </div>
+      <ax-form material>
+        <ax-form-field label="Email">
+          <ax-form-control tag="input" v-model="email" type="email"></ax-form-control>
+        </ax-form-field>
 
-      <div class="form-field">
-        <input
-          :type="password_visible ? 'text' : 'password'"
-          id="password"
-          class="form-control rounded-1"
-          :value="password"
-          @input="e => password = e.target.value"/>
-        <label for="email">Mot de passe</label>
-      </div>
+        <ax-form-field label="Password">
+          <ax-form-control tag="input" :type="password_visible ? 'text' : 'password'" v-model="password"></ax-form-control>
+        </ax-form-field>
 
-      <label class="form-check" style="display: flex; align-items: center">
-        <input type="checkbox" style="margin-right: 10px" @input="e => password_visible = e.target.checked">
-        Mot de passe visible
-      </label>
 
-      <div class="login-center-container" style="margin-top: 50px; margin-bottom: 30px">
-        Pas encore de compte
-        <router-link class="login-link" to="signup" >Inscription</router-link>
-      </div>
+        <ax-form-field>
+          <ax-form-check type="checkbox" v-model="password_visible" @click="e => password_visible = e.target.checked">
+            Mot de passe visible
+          </ax-form-check>
+        </ax-form-field>
 
-      <div class="login-center-container">
-        <button class="btn shadow-1 rounded-1 blue">Se connecter</button>
-      </div>
-    </form>
+        <div class="login-center-container" style="margin-top: 50px; margin-bottom: 30px">
+          Pas encore de compte
+          <router-link class="login-link" to="signup" >Inscription</router-link>
+        </div>
+
+        <div class="login-center-container">
+          <ax-btn class="primary shadow-1 rounded-1">Se connecter</ax-btn>
+        </div>
+      </ax-form>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "LoginView",
   data() {
@@ -73,13 +58,13 @@ export default {
   width: 100%;
   height: 100%;
 }
-.login-main-container form {
+.login-main-container .form {
   width: 500px;
   padding: 20px 20px 50px 20px;
   background: white;
   box-shadow: rgba(17, 12, 46, 0.15) 0 48px 100px 0;
 }
-.login-main-container form label {
+.login-main-container .form label {
   cursor: pointer;
 }
 .login-header {
